4a4887289aab70efb7b1ed50b4cab41a64076bc6
houdamzari <houdamzari1999@gmail.com>
2022-12-05 13:53:57 +0100

    Initialize repo

--- index.js
+++ index.js
@@ -1,58 +1,25 @@
 /* eslint-disable max-classes-per-file */
-const form = document.querySelector('form');
-const addButton = document.querySelector('.AddButton');
-const titleInput = document.querySelector('.title-input');
-const authorInput = document.querySelector('.author-input');
-const booksContainer = document.querySelector('.Books__Section');
-const listSectionBtn = document.querySelector('#list-btn');
-const formSectionBtn = document.querySelector('#new-btn');
-const contactSectionBtn = document.querySelector('#contact-btn');
-const librarySection = document.querySelector('.library-section');
-const formSection = document.querySelector('.form-section');
-const contactSection = document.querySelector('.contact-section');
 
+import {
+  form,
+  addButton,
+  titleInput,
+  authorInput,
+  booksContainer,
+  listSectionBtn,
+  formSectionBtn,
+  contactSectionBtn,
+  librarySection,
+  formSection,
+  contactSection,
+} from './modules/queries';
+
+import Library from './modules/Library';
 class Book {
   constructor(title, author, id) {
     this.title = title;
     this.author = author;
     this.id = id;
-  }
-}
-
-class Library {
-  constructor() {
-    this.bookList = [];
-    const localData = localStorage.getItem('bookList');
-    if (localData !== null) {
-      this.bookList = JSON.parse(localData);
-    }
-  }
-
-  removeBook(bookId) {
-    this.bookList = this.bookList.filter(
-      (book) => book.id !== parseInt(bookId, 10),
-    );
-  }
-
-  storeBooks() {
-    localStorage.setItem('bookList', JSON.stringify(this.bookList));
-  }
-
-  updateBooks() {
-    booksContainer.textContent = '';
-    this.bookList.forEach((book) => {
-      const booksSection = `<div class='Book__section'>
-    <div class='Book__section-item'>${book.title} by ${book.author}</div>
-    <button class='remove-button' id="${book.id}">Remove</button>
-   </div>`;
-      booksContainer.innerHTML += booksSection;
-    });
-    const removeButtons = document.querySelectorAll('.remove-button');
-    removeButtons.forEach((button) => button.addEventListener('click', (e) => {
-      this.removeBook(e.target.id);
-      this.updateBooks();
-    }));
-    this.storeBooks();
   }
 }
 
